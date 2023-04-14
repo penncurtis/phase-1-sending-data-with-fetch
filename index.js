d0a4768aa78c24c06b98915e34a4d86d1b6fd527
Penn Curtis <penn.s.m.c@gmail.com>
2023-04-14 15:25:13 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+    const newUser = {
+        name: name,
+        email: email
+    }
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+        .then(response => response.json())
+        .then(newUser => {
+            document.body.append(newUser.id)
+         })
+        .catch(error => {
+            document.body.append(error.message)
+        })
+
+}
